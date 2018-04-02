950a19924f399eadd79dff202ca1eb109e23222c
zighunter <zighunter0@gmail.com>
2018-04-02 17:20:14 -0500

    feat: develop makeGrid() function
    -working with divs and or table html

--- designs.js
+++ designs.js
@@ -3,8 +3,30 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(row, col) {
 
-// Your code goes here!
+  row = document.getElementById('inputHeight');
+  col = document.getElementById('inputWeight');
+
+  for (var n = 1; n <= row; n++) {
+    document.getElementById('pixelCanvas').innerHTML = '<div class="pixelBox"><p>1</p></div>';
+    
+   /* for (var m = 1; m > col; m++) {
+      document.getElementById('')
+    } */
+  }
 
 }
+
+/* to do 
+
+  create grid width using % screen size width and number = how tall to make a square.
+  allow the individual grid boxes to resize themselves inside
+  on click change to color in 'color picker' 
+  set default background to light tan/grey almost white
+
+
+  **
+  Optional add icon to title bar
+
+*/
