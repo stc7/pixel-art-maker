ca1f48460a7b7f13bea10afc6a23135a9ad34a2f
stc7 <scott.t.cowan@gmail.com>
2018-04-11 16:29:22 -0500

    Basic outline complete

--- designs.js
+++ designs.js
@@ -1,37 +1,25 @@
-// Select color input
-// Select size input
-
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
 
   let row = Number(document.getElementById('inputHeight').value);
   let col = Number(document.getElementById('inputWeight').value);
   let tr, td, cell;
   
-  console.log(row + col);
-
+  // Grid creator
   for (m = 0; m < row; m++) {
     tr = document.createElement('tr');
-    tr.setAttribute("class", "colorPick");
+    
     for (k = 0; k < col; k++) {
       td = document.createElement('td');
-      
-
       tr.appendChild(td);
     }
     document.getElementById('pixelCanvas').appendChild(tr);
   }
 
+  /* jQuery to select td and change color, color variable needs to be defined inside function to reset on every click*/
+  $('td').click(function fillColor() {
+    let color = document.getElementById('colorPicker').value;
+
+    $(this).attr('style') ? $(this).removeAttr('style') : $(this).attr('style', 'background-color:' + color);
+  });
 }
-
-function changeStyle() {
-  let color = document.getElementById('colorPicker').value;
-
-  let rowColor = document.getElementsByClassName("colorPick");
-  
-  rowColor.style.backgroundColor = color;
-
-}
- 
-
